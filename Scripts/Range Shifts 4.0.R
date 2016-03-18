5bf498d8d00b5acbf057e09c5defa3d37952758a
plthompson <patrickthompson9@gmail.com>
2016-03-18 16:02:15 -0600

    Updated external functions

--- Scripts/Range Shifts 4.0.R
+++ Scripts/Range Shifts 4.0.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(RColorBrewer)
 library(dplyr)
 
-source("./Functions/predict_err.r")
+source("./Functions/rShift_func.r")
 
 dispV<-c(0.0001,0.0005,0.001,0.005,0.01,0.05,0.1,0.5,1)
 reps<-2
